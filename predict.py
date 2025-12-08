db33735fe00e7aeb1f1611a7124fa4657fe42b87
ayeshatahir001 <ayeshatahir582@gmail.com>
2025-12-09 01:23:28 +0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,7 +1,6 @@
 import joblib
 import re 
 import os
-from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 
 # --------------------------
@@ -13,9 +12,12 @@ model = joblib.load("best_toxic_model.pkl")
 # --------------------------
 # PREPROCESSING
 # --------------------------
-stop_words = set(stopwords.words("english"))
-stemmer = SnowballStemmer("english")
-
+STOPWORDS = {
+    "a","an","the","is","are","am","was","were","be","been","being",
+    "i","you","he","she","it","they","them","we","us",
+    "to","for","of","and","or","in","on","at","with","as","this","that",
+    "but","if","so","because","while","by","from","about"
+}
 def remove_stopwords(text):
     return " ".join([w for w in text.split() if w not in stop_words])
 
@@ -25,8 +27,6 @@ def clean_text(text):
     text = re.sub(r"\s+", " ", text).strip()
     return text
 
-def stemming(text):
-    return " ".join([stemmer.stem(w) for w in text.split()])
 
 def preprocess(text):
     return stemming(clean_text(remove_stopwords(text)))
@@ -38,3 +38,4 @@ def predict_toxicity(text):
     vec = tfidf.transform([cleaned])
     preds = model.predict(vec)[0]
     return {label: int(value) for label, value in zip(labels, preds)}
+
